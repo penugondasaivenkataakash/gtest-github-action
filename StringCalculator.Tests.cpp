fc1e639f12897cddace5d6007836c2c74500e367
Penugonda Sai Venkata Akash <149663737+penugondasaivenkataakash@users.noreply.github.com>
2024-08-20 15:48:28 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -66,3 +66,40 @@ TEST(string_calculator_add_when_delimited_with_newline_and_comma,returns_their_s
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
+
+TEST(string_calculator_add_when_passed_a_delimiter,returns_the_sum_based_on_that_delimiter){
+ //Arrange
+  StringCalculator calculator;
+  string input="//;\n1;2";
+  int expectedsum=3;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_passed_negative_numbers,throws_an_exception_listing_invalid_values){
+  StringCalculator calculator;
+  string input="1,-2,-4,5";
+  int expectedsum=3;
+    try {
+        calculator.Add(input);
+        FAIL() << "Expected std::invalid_argument";
+    } catch (const std::invalid_argument& e) {
+        EXPECT_EQ(std::string(e.what()), "Negatives not allowed: -2,-4");
+    } catch (...) {
+        FAIL() << "Expected std::invalid_argument";
+    }
+}
+
+TEST(string_calculator_add_when_passed_numbers_over_1000,ignores_them){
+ //Arrange
+  StringCalculator calculator;
+  string input="42,1001,3";
+  int expectedsum=45;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
