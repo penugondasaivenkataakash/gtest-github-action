37e4c8360c2589eef02941c835e24352bd3187fb
Penugonda Sai Venkata Akash <149663737+penugondasaivenkataakash@users.noreply.github.com>
2024-08-20 15:11:33 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -22,3 +22,14 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_zero){
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
+
+TEST(string_calculator_add_when_passed_a_single_number,returns_1_for_one){
+ //Arrange
+  StringCalculator calculator;
+  string input="1";
+  int expectedsum=1;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
