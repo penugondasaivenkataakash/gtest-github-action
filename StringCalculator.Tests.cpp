56332ea838edeca333bef26946bc5e5a1bf3277c
Penugonda Sai Venkata Akash <149663737+penugondasaivenkataakash@users.noreply.github.com>
2024-08-20 15:16:05 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -33,3 +33,15 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_1_for_one){
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
+
+TEST(string_calculator_add_when_passed_two_comma_delimited_numbers,returns_their_sum){
+ //Arrange
+  StringCalculator calculator;
+  string input1="1";
+  string input2="2"
+  int expectedsum=3;
+  //Act
+  int actualSum=calculator.Add(input1,input2);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
