ca8c82c548b7dee50af40e66611abd7b38e221a4
Penugonda Sai Venkata Akash <149663737+penugondasaivenkataakash@users.noreply.github.com>
2024-08-21 11:15:06 +0530

    Update DataDrivenTests.cpp

--- DataDrivenTests.cpp
+++ DataDrivenTests.cpp
@@ -15,6 +15,7 @@ class string_calculator_add_Parameter_Fixture:
 INSTANTIATE_TEST_SUITE_P(SingleNumerParameter,
                          string_calculator_add_Parameter_Fixture,
                          testing::Values(
+                                        std::make_tuple("",0),
                                         std::make_tuple("0",0),
                                         std::make_tuple("1", 1),
                                         std::make_tuple("1,2",3),
@@ -38,14 +39,9 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_empty_strin
   ASSERT_EQ(actualSum,expectedsum);
 }
 
-TEST_F(string_calculator_add_Fixture,when_passed_a_single_number_returns_0_for_zero){
- //Arrange
+TEST_F(StringCalculatorAddTestFixture, string_calculator_add_when_passed_negative_number_throws_an_exception_listing_invalid_values){
   string input="1,-2,-4,5";
-  int expectedsum=3;
-  //Act
-  int actualSum=calculator.Add(input);
-  //Assert
-  EXPECT_EQ(actualSum,expectedsum);
+  ASSERT_THROW(calculator.Add(input),invalid_argument);
 }
 
 TEST_P(string_calculator_add_Parameter_Fixture,when_passed_a_single_Or_Multiple_Number_with_Delimiters_returns_Sum){
