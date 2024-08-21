10047622cf3826e1c78cacb89426b34de45a4d8a
Penugonda Sai Venkata Akash <149663737+penugondasaivenkataakash@users.noreply.github.com>
2024-08-21 11:15:21 +0530

    Update DataDrivenTests.cpp

--- DataDrivenTests.cpp
+++ DataDrivenTests.cpp
@@ -28,17 +28,6 @@ INSTANTIATE_TEST_SUITE_P(SingleNumerParameter,
                                         std::make_tuple("//[**][%^]\n4**1%^9",14)
   ));
 
-TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_empty_string){
- //Arrange
-  StringCalculator calculator;
-  string input="";
-  int expectedsum=0;
-  //Act
-  int actualSum=calculator.Add(input);
-  //Assert
-  ASSERT_EQ(actualSum,expectedsum);
-}
-
 TEST_F(StringCalculatorAddTestFixture, string_calculator_add_when_passed_negative_number_throws_an_exception_listing_invalid_values){
   string input="1,-2,-4,5";
   ASSERT_THROW(calculator.Add(input),invalid_argument);
